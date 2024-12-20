6053581f8f89c8534ed95aad3b9282a3db754448
MalsriManaram <malsrimanaram7878@gmail.com>
2024-12-20 09:03:50 +0530

    update

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,7 +1,7 @@
 const { stack } = require("../app");
 
 // use to add all the error handlers
-const errorHandler = (error, res, req, next) => {
+const errorHandler = (error, req, res, next) => {
     const code = res.code ? res.code : 500;
 
     res.status(code).json({ code, status: false, message: error.message, stack: error.stack});
